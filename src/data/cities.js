7ba082183126ccd832cec3ea1a5d433f0065a466
zincel <106347425+zincel@users.noreply.github.com>
2022-11-02 17:41:05 -0300

    se armo los array

---
+++ src/data/cities.js
@@ -0,0 +1,98 @@
+let cities = [
+    {
+        id: "roma",
+        name: "Roma",
+        continent: "Europa",
+        photo: "https://content.r9cdn.net/rimg/dimg/7e/77/5158e754-city-25465-1525fd406f5.jpg?width=1750&height=1000&xhint=1208&yhint=1108&crop=true",
+        population: 38000000,
+        userId: "adm1"
+    },
+    {
+        id: "paris",
+        name: "París",
+        continent: "Europa",
+        photo: "https://viajes.nationalgeographic.com.es/medio/2022/07/13/paris_37bc088a_1280x720.jpg",
+        population: 19000000,
+        userId: "adm1"
+    },
+    {
+        id: "buenosAires",
+        name: "Buenos Aires",
+        continent: "America",
+        photo: "https://www.buenosaires.gob.ar/sites/gcaba/files/al_avenida_9_dejulio.jpg",
+        population: 10000000,
+        userId: "adm1"
+    },
+    {
+        id: "barcelona",
+        name: "Barcelona",
+        continent: "Europa",
+        photo: "https://img2.rtve.es/imagenes/ciudades-para-siglo-xxi-barcelona-ciudad-vertebrada-2/1561977573130.jpg",
+        population: 18500000,
+        userId: "adm2"
+    },
+    {
+        id: "estambul",
+        name: "Estambul",
+        continent: "Europa",
+        photo: "https://elviajerofeliz.com/wp-content/uploads/2019/05/Que%CC%81-ver-en-Estambul.jpg",
+        population: 13400000,
+        userId: "adm2"
+    },
+    {
+        id: "londres",
+        name: "Londres",
+        continent: "Europa",
+        photo: "https://elviajerofeliz.com/wp-content/uploads/2017/10/Rutas-por-Londres.jpg",
+        population: 19600000,
+        userId: "adm2"
+    },
+    {
+        id: "nuevaYork",
+        name: "Nueva York",
+        continent: "America",
+        photo: "https://elviajerofeliz.com/wp-content/uploads/2017/08/Qu%C3%A9-ver-en-Nueva-York-5-cosas-que-no-te-puedes-perder.gif",
+        population: 14000000,
+        userId: "adm3"
+    },
+    {
+        id: "madrid",
+        name: "Madrid",
+        continent: "Europa",
+        photo: "https://www.rookieintown.com/wp-content/uploads/zonas-barrios-madrid.jpg",
+        population: 4900000,
+        userId: "adm3"
+    },
+    {
+        id: "amsterdam",
+        name: "Amsterdam",
+        continent: "Europa",
+        photo: "https://media.traveler.es/photos/6231abc7d03e1c5549e648ca/16:9/w_2560%2Cc_limit/The%2520Best%2520Places%2520for%2520Female%2520Solo%2520Travelers_Amsterdam_GettyImages-923546342.jpg",
+        population: 17000000,
+        userId: "adm3"
+    },
+    {
+        id: "marDelPlata",
+        name: "Mar del Plata",
+        continent: "America",
+        photo: "https://revistauncamino.com.ar/wp-content/uploads/2018/12/portada-Mar-del-Plata-la-hisotria-de-la-feliz-scaled.jpg",
+        population: 4600000,
+        userId: "adm4"
+    },
+    {
+        id: "tokio",
+        name: "Tokio",
+        continent: "Asia",
+        photo: "https://media.admagazine.com/photos/618a5ef1a8ad6c5249a74d1d/4:3/w_2000,h_1500,c_limit/91683.jpg",
+        population: 10440000,
+        userId: "adm4"
+    },
+    {
+        id: "florencia",
+        name: "Florencia",
+        continent: "",
+        photo: "https://viajes.nationalgeographic.com.es/medio/2018/02/27/mapa-florencia__1280x720.jpg",
+        population: 5125700,
+        userId: "adm4"
+    }
+]
---
+++ src/data/registeredUsers.js
@@ -0,0 +1,46 @@
+let users = [
+    {
+        id: "user1",
+        name: "Luis",
+        lastName: "Lopez",
+        age: 22,
+        email: "luislopez@hotmail.com",
+        password: "hello123",
+        code: "q87p1e",
+        verifled: "true",
+        logged: "true"
+    },
+    {
+        id: "user2",
+        name: "Marcos",
+        lastName: "Torres",
+        age: 32,
+        email: "marcostorres@hotmail.com",
+        password: "baby456",
+        code: "q56p9e",
+        verifled: "true",
+        logged: "true"
+    },
+    {
+        id: "user3",
+        name: "Daniel",
+        lastName: "Santos",
+        age: 47,
+        email: "danielsantos@hotmail.com",
+        password: "bye789",
+        code: "q31p8e",
+        verifled: "true",
+        logged: "true"
+    },
+    {
+        id: "user4",
+        name: "Patricio",
+        lastName: "Barrios",
+        age: 25,
+        email: "patriciobarrios@hotmail.com",
+        password: "tok123",
+        code: "q46p8e",
+        verifled: "true",
+        logged: "true"
+    }
+]
