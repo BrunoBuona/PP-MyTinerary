36f9d28e926101a63e7b30bcbf30bb5356982b67
zincel <106347425+zincel@users.noreply.github.com>
2022-11-04 22:47:34 -0300

    MT-F2-05

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import {LoginForm} from "./components/LoginForm/LoginForm";
 import {NotFound} from "./components/404/NotFound.jsx";
+import {Home} from "./layouts/Home"
+import {Hoome} from "./pages/Hoome"
 
 function App() {
   return (
-      <>
+    <>
+    <Home>
       <Routes>
-        <Route path="/login" element={<LoginForm/>} />
+        <Route path="/" element={<Hoome/>}/>
+        <Route path="/login" element={<LoginForm/>}/>
         <Route path="*" element={<NotFound/>} />
       </Routes>
+    </Home>
     </>
   );
 }
 
 export default App;
-
