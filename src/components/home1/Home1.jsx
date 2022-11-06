b34589e0b5dbccd5fd11d88279bfd0daedb2c629
zincel <106347425+zincel@users.noreply.github.com>
2022-11-06 02:19:11 -0300

    FIX-MT-F1-03

--- src/components/home1/Home1.jsx
+++ src/components/home1/Home1.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CallToAction from './CallToAction/CallToAction'
+import { Link as NavLink } from "react-router-dom";
 
 export default function Home1() {
   return (
@@ -8,8 +9,13 @@ export default function Home1() {
             <h1>My-Itinerary</h1>
             <h2>¡Amazing Travels at Low Cost!</h2>
             <div className="cta-container">
-                <CallToAction name="Cities" Link="/"/>
-                <CallToAction name="Hotels" Link="/"/>
+            <NavLink className="cta-container" to="/cities">
+              <CallToAction name="Cities" />
+            </NavLink>
+            <NavLink className="cta-container" to="/hotels">
+              <CallToAction name="Hotels" />
+            </NavLink>
+                
             </div>
         </div>
     </main>
