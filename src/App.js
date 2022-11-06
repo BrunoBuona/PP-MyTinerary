f1c6c119e1427fc6aba1d880749851aa107e434e
zincel <106347425+zincel@users.noreply.github.com>
2022-11-05 23:25:54 -0300

    FIX-MT-F1-07

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<LoginForm/>}/>
         <Route path="*" element={<NotFound/>} />
-        <Route path="/sigup" element={<SignUp/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
       </Routes>
     </Main>
     </>
