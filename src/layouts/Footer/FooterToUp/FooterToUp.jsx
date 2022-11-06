03b478975ca5f74384c3290341a5a25120cef249
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-05 23:29:00 -0300

    FIX-MT-F2-04

--- src/layouts/Footer/FooterToUp/FooterToUp.jsx
+++ src/layouts/Footer/FooterToUp/FooterToUp.jsx
@@ -3,9 +3,9 @@ import '../Footer.css'
 function FooterToTop() {
     return(
     <div className="toTop">
-        <a href="#" class="btn-flotante">▲</a>
-
-    </div>)
+        <a href="#" clasNames="btn-flotante">▲</a>
+    </div>
+    )
 }
 
 export { FooterToTop }
