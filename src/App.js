e951b55f4bfc929fcce5d3452684e50ed09486ea
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-05 23:18:17 -0300

    FIX-MT-F2-06

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import {LoginForm} from "./components/LoginForm/LoginForm";
 import {NotFound} from "./components/404/NotFound.jsx";
-import {Main} from "./layouts/Main"
+import {Main} from "./layouts/Main.jsx"
 import {Home} from "./pages/Home"
 import {SignUp} from "./pages/signUp/SignUp"
 
