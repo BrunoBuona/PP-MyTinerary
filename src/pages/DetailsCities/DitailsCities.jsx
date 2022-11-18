60a8bab1c6d4a42340a79af7a477ed07542835ca
zincel <106347425+zincel@users.noreply.github.com>
2022-11-18 19:22:25 -0300

    fix general sprint-2

--- src/pages/DetailsCities/DitailsCities.jsx
+++ src/pages/DetailsCities/DitailsCities.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useState } from 'react'
-
+import {BASE_URL} from '../../api/url' 
 
 const DitailsCities = () => {
 
@@ -14,11 +14,11 @@ const DitailsCities = () => {
     let { id } = useParams()
 
     useEffect(() => {
-        axios.get(`http://localhost:8000/api/cities/${id}`)
+        axios.get(`${BASE_URL}/api/cities/${id}`)
             .then(res => setCiti(res.data.data))
             .catch((error) => console.log(error))
 
-        axios.get(`http://localhost:8000/api/itineraries?cityId=${id}`)
+        axios.get(`${BASE_URL}/api/itineraries?cityId=${id}`)
             .then(res => setItinerarios(res.data.response))
             .catch((error) => console.log(error))
     }, [id])
@@ -30,7 +30,7 @@ console.log(itinerarios);
                     <div className="main-space">
                         <div className='MainHotel'>
                             <div className='left-mh'>
-                                <img className='img-mh' src={citi.photo} alt="Hotel Photo" />
+                                <img className='img-mh' src={citi.photo} alt={citi.name} />
                             </div>
                             <div className='right-mh'>
                                 <h1>{citi.name} </h1>
@@ -38,45 +38,31 @@ console.log(itinerarios);
                             </div>
                         </div>
                     </div>
-                    <div>
+                </div>
+            {itinerarios.map((e) => {
+                return(
+                                    <div>
                     <div className='main-details-City'>
                         <h3 className='h3-actividades'>Activity!</h3>
                         <div className="main-space">
                             <div className='MainHotel'>
                                 <div className='left-mh'>
-                                    <img class='img-mh' src={itinerarios[0].photo[0]} alt={itinerarios.name} />
-                                    <img class='img-mh' src={itinerarios[0].photo[1]} alt={itinerarios.name} />
-                                    <img class='img-mh' src={itinerarios[0].photo[2]} alt={itinerarios.name} />
+                                    <img class='img-mh' src={e.photo[0]} alt={e.name} />
+                                    <img class='img-mh' src={e.photo[1]} alt={e.name} />
+                                    <img class='img-mh' src={e.photo[2]} alt={e.name} />
                                 </div>
                                 <div className='right-mh'>
-                                    <h3>{itinerarios[0].name}</h3>
-                                    <p>{itinerarios[0].description}</p>
-                                    <p>Price: {itinerarios[0].price} $USD</p>
+                                    <h3>{e.name}</h3>
+                                    <p>{e.description}</p>
+                                    <p>Price: {e.price} $USD</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div className='main-details-City'>
-                        <h3 className='h3-actividades'>Activity!</h3>
-                        <div className="main-space">
-                            <div className='MainHotel'>
-                                <div className='left-mh'>
-                                    <img class='img-mh' src={itinerarios[1].photo[0]} alt={itinerarios[1].name} />
-                                    <img class='img-mh' src={itinerarios[1].photo[1]} alt={itinerarios[1].name} />
-                                    <img class='img-mh' src={itinerarios[1].photo[2]} alt={itinerarios[1].name} />
-                                </div>
-                                <div className='right-mh'>
-                                    <h3>{itinerarios[1].name}</h3>
-                                    <p>{itinerarios[1].description}</p>
-                                    <p>Price: {itinerarios[1].price} $USD</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
+
+                )
+            })}
             </>
         )
     }
