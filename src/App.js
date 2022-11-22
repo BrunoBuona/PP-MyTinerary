f093428421a1aeb0c5c05059884846e83f350916
zincel <106347425+zincel@users.noreply.github.com>
2022-11-22 08:53:00 -0300

    MT-01-41

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {DetailsH} from "./components/DetailsHotel/DetailsHotels"
 import {Cities3} from "./pages/Cities/Cities"
 import DetailsC from "./pages/DetailsCities/DitailsCities"
 import MyHotels from "./pages/myHotels/myHotels";
+import MyCity from "./pages/myCity/MyCity"
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="/cities" element={<Cities3/>}/>
         <Route path="/detailscities/:id" element={<DetailsC/>}/>
         <Route path="/myhotels" element={<MyHotels/>}/>
+        <Route path="/myCities" element={<MyCity/>}/>
       </Routes>
     </Main>
     </>
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,12 +2,14 @@ import hotelReducer from "./hotelReducer"
 import cityReducer from "./cityReducer"
 import CityFilterReducer from "./cityFilterReducer"
 import myHotelsReducer from "./myHotelReducer"
+import myCityReducer from "./myCityReducer"
 
 const rootReducer = {
     hotelReducer,
     myHotelsReducer,
     cityReducer,
-    CityFilterReducer
+    CityFilterReducer,
+    myCityReducer
 }
 
 export default rootReducer;
