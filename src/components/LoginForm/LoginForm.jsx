e81a784fb8dd433d4259fd7e83b1c9f450908b8e
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-26 16:18:44 -0300

    MT-02-46

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,48 +6,38 @@ import { useSelector } from "react-redux";
 import { BASE_URL } from "../../api/url";
 import { useDispatch } from "react-redux";
 import loginAction from "../../redux/actions/loginForm";
+import { useNavigate } from "react-router-dom";
 const Swal = require("sweetalert2");
 
 function LoginForm() {
   const email = useRef();
   const pwd = useRef();
+  const navigate = useNavigate();
   let store = useSelector((state) => state.loginReducer);
   console.log(store)
   const dispatch = useDispatch()
-  async function Submit(e) {
+  async function submit(e) {
     e.preventDefault();
     const dataLogin = {
       email: email.current.value,
       password: pwd.current.value,
     };
-    let res = await axios.post(`${BASE_URL}/api/auth/signin`, dataLogin);
     try {
+      let res = await axios.post(`${BASE_URL}/api/auth/signin`, dataLogin)
       if (res.data.success) {
       let tokenx = res.data.response.token
       dispatch(loginAction.getToken(tokenx))
       localStorage.setItem("token", tokenx)
-      let timerInterval
+      let timerInterval;
       Swal.fire({
         title: 'Successfully Login',
         html: "We're redirecting you to Home Page...",
         timer: 2200,
         timerProgressBar: true,
-        didOpen: () => {
-          Swal.showLoading()
-          const b = Swal.getHtmlContainer().querySelector('b')
-          timerInterval = setInterval(() => {
-            b.textContent = Swal.getTimerLeft()
-          }, 100)
-        },
         willClose: () => {
-          window.location.href = `/`
+          navigate('/')
         }
-      }).then((result) => {
-        /* Read more about handling dismissals below */
-        if (result.dismiss === Swal.DismissReason.timer) {
-          console.log('I was closed by the timer')
-        }
-        });
+      })
     } else {
       Swal.fire({
         icon: "error",
@@ -55,17 +45,17 @@ function LoginForm() {
         text: `Errors: ${res.data.message.join(", ")}`,
       });
     }
-  } catch (err) {
+  }catch(err){
     Swal.fire({
       icon: "error",
       title: "We found an error...",
       text: `Errors: You have entered an invalid email or password.`,
-    });
+    })
   }
 }
 return (
   <>
-      <form className="form" onSubmit={e=> Submit(e)}>
+      <form className="form" onSubmit={e=> submit(e)}>
         <div className="form-body">
           <h1 className="title">MyTinerary</h1>
           <h2 className="title2">Sign In</h2>
--- src/redux/actions/loginForm.js
+++ src/redux/actions/loginForm.js
@@ -3,10 +3,14 @@ import axios from "axios";
 import {BASE_URL} from "../../api/url";
 
 let getToken= createAsyncThunk('getToken', async(tokenx)=>{
+    try{
     let headers = {headers:{'Authorization': `Bearer ${tokenx}`}}
     let token = await axios.post(`${BASE_URL}/api/auth/token`, {}, headers)
     let res = token.data.response.user
     return(res)
+    }catch(err){
+        console.log(err)
+    }
 })
 
 const loginAction = {
